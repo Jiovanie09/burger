15548fddec2a4747fad101bf8548ffeb448b486e
Jiovanie Martinez <jiovaniemartinez@gmail.com>
2020-04-17 12:52:13 -0500

    connection pass

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) value ("Regular Cheese Burger");
+insert into burgers (burger_name) value ("Double Bacon and Cheese Burger");
+insert into burgers (burger_name) value ("Double Bacon with Jalapenos Cheese Burger");
